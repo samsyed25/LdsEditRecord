6ef48f68d087352c52bf2f6d6abd3daaf83322cf
samsyed25 <147829203+samsyed25@users.noreply.github.com>
2023-10-24 14:30:36 +0530

    Create LdsEditRecord.js

---
+++ LdsEditRecord.js
@@ -0,0 +1,10 @@
+import { LightningElement , api} from 'lwc';
+
+export default class LdsEditRecord extends LightningElement {
+    @api recordId;
+    @api objectApiName;
+
+    handleSubmit(){}
+
+    handleSuccess(){}
+}
